47fd03b424e454890ef71dd13a43c7c54ceb6d58
Roopa Devan <Roopa Devan@ROOPA>
2019-09-16 23:43:10 +0530

    changes done in 1st branch pushed in 1st branch

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMySqlApplication {
+	//Changes done in first Branch
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMySqlApplication.class, args);
